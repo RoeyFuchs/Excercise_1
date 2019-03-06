3a8ef5a4c4e607e67fc017d01edfeb1c1e87d70a
Roey Fuchs <roey.fuchs@live.biu.ac.il>
2019-03-06 12:58:01 +0200

    Dicentery and single mission updates

--- Excercise_1/MissionContainer.cs
+++ Excercise_1/MissionContainer.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Excercise_1
 {
-  
+    public delegate double aFunc(double val);
     public class FunctionsContainer
     {
-       
+        private Dictionary<string, aFunc> dic;
+
+        public aFunc this[string str]
+        {
+            get {
+                //check if we have the key and return the value (the function)
+                if(dic.ContainsKey(str)) {
+                    return dic[str];
+                }
+                //if we don't have the key in the dictinary, return a deafult function 
+                return (val => val);
+            }
+            //add a function to the dictionary
+            set { dic[str] = value; }
+        }
+
+        FunctionsContainer()
+        {
+            dic = new Dictionary<string, aFunc>();
+        }
+
+
 
     }
+}
+
+
 }
--- Excercise_1/SingleMission.cs
+++ Excercise_1/SingleMission.cs
@@ -8,6 +8,21 @@ namespace Excercise_1
 {
     public class SingleMission : IMission
     {
-      
+        private const string TypeString = "Single";
+        private aFunc calculateFunc;
+
+        SingleMission(aFunc calcFunc, string name)
+        {
+            calculateFunc = calcFunc;
+            Name = name;
+            Type = TypeString;
+        }
+
+        event EventHandler<double> OnCalculate;  // An Event of when a mission is activated
+
+        String Name { get; }
+        String Type { get; }
+
+        double Calculate(double value) {}
     }
 }
